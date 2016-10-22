fea271b9dccf0b71e26981aca4e8e301f6544406
DieUbermensch <mikkel_krogh_simonsen@hotmail.com>
2016-10-22 19:52:25 +0200

    Trying to added the DSP code

---
+++ Code/DSP/ActiveNoiseCancelling/main.c
@@ -0,0 +1,18 @@
+//***************************************************************************************
+//  Hello world Example
+//
+//  Description: This simple program outputs hello world to the console
+//
+//  J. Stevenson
+//  Texas Instruments, Inc
+//  October 2008
+//  Built with Code Composer Studio v4
+//***************************************************************************************
+
+#include <stdio.h>						// standard I/O needed for printf
+
+void main(void) {
+	printf("hello world\n");
+	
+	return;
+}
