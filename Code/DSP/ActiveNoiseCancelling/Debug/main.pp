b36a2001970eed4aad507c1dfcef0adc36b5cda2
DieUbermensch <mikkel_krogh_simonsen@hotmail.com>
2016-10-26 17:29:16 +0200

    removed useless files

--- Code/DSP/ActiveNoiseCancelling/Debug/main.pp
+++ Code/DSP/ActiveNoiseCancelling/Debug/main.pp
@@ -1,7 +1,11 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/loopbackcfg.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/loopbackcfg.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/std.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdint.h
 
 ../main.c: 
-C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/c5500/include/loopbackcfg.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/loopbackcfg.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/std.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdint.h: 
