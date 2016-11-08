db4ba2484ef88f45e0dabffd3a2a58698a0fd3cd
DieUbermensch <mikkel_krogh_simonsen@hotmail.com>
2016-11-08 21:27:54 +0100

    adding my changes

--- Code/DSP/aic3204/Debug/main.pp
+++ Code/DSP/aic3204/Debug/main.pp
@@ -1,23 +1,23 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/stdio.h
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/linkage.h
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/stdarg.h
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515.h
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515_gpio.h
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515.h
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515_i2c.h
-main.obj: C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdio.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/linkage.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdarg.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515_gpio.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515_i2c.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h
 main.obj: ../aic3204_setup.h
 
 ../main.c: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/stdio.h: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/linkage.h: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/stdarg.h: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515_gpio.h: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515_i2c.h: 
-C:/Program\ Files/TexasInstruments/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdio.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/linkage.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdarg.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515_gpio.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515_i2c.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
 ../aic3204_setup.h: 
