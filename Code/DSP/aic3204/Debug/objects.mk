3912ac42a59107fe81e6a576e1a7138ee827c484
Kiis <kkje13@student.aau.dk>
2016-11-09 17:35:26 +0100

    firfinish

--- Code/DSP/aic3204/Debug/objects.mk
+++ Code/DSP/aic3204/Debug/objects.mk
@@ -2,6 +2,6 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-LIBS := "$(GEN_CMDS_QUOTED)" -l"usbstk5515bsl.lib" -l"rts55x_eh.lib"
+LIBS := "$(GEN_CMDS_QUOTED)" -l"usbstk5515bsl.lib" -l"rts55x.lib"
 
 USER_OBJS :=
--- Code/DSP/aic3204/FIRFunction.asm
+++ Code/DSP/aic3204/FIRFunction.asm
@@ -1,9 +1,26 @@
-	.global _CPFIR;
+	.global _FIR;
 
-_CPFIR:	
-	MOV #0, AC1					; Clear AC1
-	MOV #51, BK03	
-RET
+;------------------------------------------------------
+;		void FIR(Int16 *x,	=> AR0
+;			   	   Int16 *b, 	=> AR1
+;				   Int16 N, 	=> T0
+;				   Int16 i)		=> T1
+
+
+
+_FIR:	
+	MOV #0, AC0					; Clear AC1
+	MOV mmap(AR0),BSA01			; Set base Adress for buffer
+	MOV mmap(T0), BK03			; Set buffer size to filter order
+	MOV *(#T1),AR0				; Start from ith sample
+	bset AR0LC					; Set pointer as circular
+	SUB #1,T0					; Setup Repititions
+	MOV T0,CSR
+	RPT CSR					
+	MAC *AR0-,*AR1+,AC0 		; Do filter convolution
+	SFTL AC0, #-15				; Bit shift to fit Q15
+	MOV AC0,T0					; Output sample
+    RET
 
 
 	
