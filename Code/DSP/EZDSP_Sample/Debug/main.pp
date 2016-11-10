48b52a57a14a986f2a01c95993f7b189c124f49c
DieUbermensch <mikkel_krogh_simonsen@hotmail.com>
2016-11-10 14:19:32 +0100

    i have made some useless changes

---
+++ Code/DSP/EZDSP_Sample/Debug/main.pp
@@ -0,0 +1,37 @@
+# FIXED
+
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/src/main.c
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdio.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/linkage.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdarg.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/data_types.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/register_system.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/register_cpu.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/rtc.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/control.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/i2s.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/dma.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/timer.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/lcd.h
+main.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/sar.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515_led.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h
+
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/src/main.c: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdio.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/linkage.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/stdarg.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/data_types.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/register_system.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/register_cpu.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/rtc.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/control.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/i2s.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/dma.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/timer.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/lcd.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/sar.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515_led.h: 
+C:/Program\ Files\ (x86)/Texas\ Instruments/cssv424/ccsv4/tools/compiler/c5500/include/usbstk5515.h: 
---
+++ Code/DSP/EZDSP_Sample/Debug/timer.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+timer.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/src/timer.c
+timer.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/data_types.h
+timer.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/timer.h
+timer.obj: C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/register_cpu.h
+
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/src/timer.c: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/data_types.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/timer.h: 
+C:/Users/Mikkel\ Krogh/Downloads/usbstk5515_v1/tests/USB_Stick_Sample/inc/register_cpu.h: 
