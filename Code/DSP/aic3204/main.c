f1cbacd17e8c549bcfcf3a1becd47956d2a70d82
Kiis <kkje13@student.aau.dk>
2016-11-10 14:21:30 +0100

    FXLMS

--- Code/DSP/aic3204/main.c
+++ Code/DSP/aic3204/main.c
@@ -23,25 +23,32 @@
 
 
 // Initialize
+void initAll(void);
+Int16 data1 = 0, data2 = 0, data3 = 0, data4 = 0;
 
-
-
-Int16 data1, data2, data3, data4;
-Int16 k;
-Int16 delayVar1;
 //FIR
-#define N 10
-
+extern Int16 FIR(Int16 *x, Int16 *b, Int16 order, Int16 index);
+#define N 5
 Int16 x[N];
-//Int16 b[N];
 Int16 b[] = {0xCCD,0xCCD,0xCCD,0xCCD,0xCCD,0xCCD,0xCCD,0xCCD,0xCCD,0xCCD};
 //Int16 b[] = {1,0,0,0,0,0,0,0,0,0};
 Int16 y[N];
 Int16 i = 0;
 
-extern Int16 FIR(Int16 *x, Int16 *b, Int16 order, Int16 index);
-extern void  FXLMS(Int16 *x, Int16 *b, Int order, Int16 error, Int16 mu, Int16 index)
-void initAll(void);
+//FXLMS
+extern void  FXLMS(Int16 *x, Int16 *b, Int16 error, Int16 index, Int16 *uN );
+#define NLMS 5
+Int16 xLMS[NLMS] = {0x4000,0,0,0,0};
+Int16 bLMS[NLMS] = {0x4000,2,3,4,5};
+Int16 u = 1; //Is multiplied in initialize function
+Int16 iLMS = 0;
+Int16 e = 1;
+Int16 constants[2];
+
+
+
+
+
 
 void main( void )
 {
@@ -52,27 +59,33 @@ void main( void )
 
             while ( 1 )
             {
-            	if (I2S0_IR == 0)
             	/* Read Digital audio */
             	while((Rcv & I2S0_IR) == 0);  // Wait for interrupt pending flag
-                k=100;
                 data3 = I2S0_W0_MSW_R;  // 16 bit left channel received audio data
       	        data1 = I2S0_W0_LSW_R;
       	        data4 = I2S0_W1_MSW_R;  // 16 bit right channel received audio data
       	        data2 = I2S0_W1_LSW_R;
 
       	        //asm(" bset XF");
+      	        /* Signal processing */
       	        asm(" bclr XF");
       	        
       	        if(i == N)
       	        {
       	        	i = 0;
       	        }
-      	        x[i] = data3;
-       	        y[i] = FIR(x,b,N,i);
+      	        if(iLMS == NLMS)
+      	        {
+      	        	iLMS = 0;
+      	        }
+      	        //xLMS[i] = data3;
+       	        //xLMS[iLMS] = FIR(x,b,N,i);
+       	        FXLMS(xLMS, bLMS, e, iLMS, constants);
+       	        
        	        data3 = y[i];
        	        data4 = y[i];      	               
        	        i++;
+       	        iLMS++;
       	        asm(" bset XF");
 
 				/* Write Digital audio */
@@ -89,11 +102,18 @@ void main( void )
 void initAll(void)
 {
 	Int16 cnt;
-	// Clear array
+	// Clear arrays
 	for(cnt=0;cnt<N;cnt++) 
 	{
 		x[cnt] = 0;
-		//b[cnt] = 0;
 		y[cnt] = 0;	
 	}	
+	for(cnt=0;cnt<NLMS;cnt++) 
+	{
+		//xLMS[cnt] = 0;
+		//bLMS[cnt] = 0;	
+	}	
+	u = -2*u;
+	constants[0] = u;
+	constants[1] = NLMS;
 }
