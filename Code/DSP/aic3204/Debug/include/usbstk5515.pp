ea62e8ec00fa49f3492154707877555633f02f88
DieUbermensch <mikkel_krogh_simonsen@hotmail.com>
2016-11-28 18:21:32 +0100

    updating ignore list

--- Code/DSP/aic3204/Debug/include/usbstk5515.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-include/usbstk5515.obj: ../include/usbstk5515.c
-include/usbstk5515.obj: ../include/usbstk5515.h
-
-../include/usbstk5515.c: 
-../include/usbstk5515.h: 
--- Code/DSP/aic3204Slave/Debug/include/usbstk5515_gpio.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-include/usbstk5515_gpio.obj: ../include/usbstk5515_gpio.c
-include/usbstk5515_gpio.obj: ../include/usbstk5515_gpio.h
-include/usbstk5515_gpio.obj: ../include/usbstk5515.h
-
-../include/usbstk5515_gpio.c: 
-../include/usbstk5515_gpio.h: 
-../include/usbstk5515.h: 
